8e7cad9402e21a38a5c94ebd43faa44edd20e3b0
alisowonciki <166418920+alisowonciki@users.noreply.github.com>
2024-11-04 11:47:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'o que é um contrabaixo?',
+    'como tocar?',
+    'é um intrumento que tem 4 ou 6 cordas'
+)
+criaCartao(
+    'Escolha dificil',
+    'Entre palheta ou moeda qual voce iria?',
+    'palheta sempre!!'
+)
+    
+criaCartao(
+    'carros',
+    'qual o cambio mais rapido manual ou automatico?',
+    'o cambio manual sempre é mais rapido'
+)
+    
+criaCartao(
+    'cambio',
+    'existe um cambio mais rapido que o manual?',
+    'sim apenas um cambio é o mais rapido que é o cambio PDK da porsche'
+)
+criaCartao(
+    'Informações sobre o free fire',
+    'Como e os modos de partidas ',
+    'Os modos são separados como partidas rankeadas e classicas, pode ser tanto como 2v2 4v4 ou solo, e <br> tambem pode se criar salas para ser fogo amigo'
+)
+    
+
+
